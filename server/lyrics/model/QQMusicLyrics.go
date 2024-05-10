3cbee64087ba509b310e8ba822a320be63013a94
chenaiquan <like.aiquan@qq.com>
2024-05-10 23:43:22 +0800

    reorganize package structure

--- server/lyrics/model/QQMusicLyrics.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type QQMusicLyrics struct {
-	Retcode int    `json:"retcode"`
-	Code    int    `json:"code"`
-	Subcode int    `json:"subcode"`
-	Lyric   string `json:"lyric"`
-	Trans   string `json:"trans"`
-}
--- server/lyrics/provider/Provider.go
+++
@@ -1,8 +0,0 @@
-package provider
-
-import "lyrics/model"
-
-type Provider interface {
-	// Lyrics Base64 字符串
-	Lyrics(request model.SearchRequest) []model.Result
-}
